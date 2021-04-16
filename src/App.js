09a34c675d9971c509f4effa17f3a504f0e39df3
Ahmed <ahmedtariqlife900@gmail.com>
2021-04-16 18:17:43 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    return fetch("https://www.solutionsplayer.com/wp-json/wp/v2/posts/")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          posts: responseJson,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.posts.map((item, index) => {
+          return (
+            <div>
+              <div class="row">
+                <div class="leftcolumn">
+                  <div class="card">
+                    <div className="title">
+                      <h1>{item.title.rendered}</h1>
+                    </div>
+                    <div
+                      className="content"
+                      dangerouslySetInnerHTML={{
+                        __html: item.content.rendered,
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
